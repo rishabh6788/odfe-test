1878a7d00ba322731e58cb3f87d55adbe7d0970f
rishabh6788 <rishabhksingh@gmail.com>
2019-12-09 12:50:40 +0530

    refactoring changes

--- odfe-test/fixtures.py
+++ odfe-test/fixtures.py
@@ -1,6 +1,5 @@
 from pytest import fixture
 import requests
-from requests import Timeout, ConnectionError, ConnectTimeout
 import random, string
 from test_data import *
 import json
@@ -11,17 +10,14 @@ print(index_name)
 
 @fixture()
 def get_request():
-    try:
-        r = requests.get(url=es_url, auth = ('admin', 'admin'), verify = False, timeout = 3)
-    except ConnectTimeout as e:
-        return e
+    r = create_get_request_obj(node_url=es_url)
     return r
 
 
 @fixture()
 def get_cat_nodes():
     node_url = es_url + '_cat/nodes?v'
-    r = requests.get(url=node_url, auth = ('admin', 'admin'), verify = False, timeout = 3)
+    r = create_get_request_obj(node_url=node_url)
     return r
 
 
@@ -44,3 +40,8 @@ def load_test_data():
     node_url = es_url + index_name + '/' + '_doc/'
     r = requests.post(url=node_url, params=params, auth = ('admin', 'admin'), verify = False, headers=req_headers, data=json.dumps(test_data))
     return r
+
+
+def create_get_request_obj(node_url, params=None, headers=None):
+    req = requests.get(url=node_url, auth = ('admin', 'admin'), verify = False, timeout = 3, params=params, headers=headers)
+    return req
